0fb1161e03c78ab475ffc9fc7c2b184c70786a76
Mahin05 <mahin05@gmail.com>
2022-04-17 23:04:56 +0600

    readme edited

--- src/Pages/AboutMe/AboutMe.js
+++ src/Pages/AboutMe/AboutMe.js
@@ -7,10 +7,10 @@ const AboutMe = () => {
         <div>
             <h1 className='mt-4'>About me</h1>
             <div className='about-style container'>
-                <img src={myimg} alt="" />
+                <img className='about-img' src={myimg} alt="" />
                 <div>
-                    <h2>Name: <span>Mohammad Minhajul Islam Mahin</span> </h2>
-                    <h2>My Goal: <span>My goal is to become a skilled web developer. In starting I want to join in any software company as a web developer and want to build up my career.</span> </h2>
+                    <h2 className='about-h2'>Name: <span>Mohammad Minhajul Islam Mahin</span> </h2>
+                    <h2 className='about-h2'>My Goal: <span>My goal is to become a skilled web developer. In starting I want to join in any software company as a web developer and want to build up my career.</span> </h2>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import './Service.css'
 
-const Service = ({service}) => {
-    const {id,name,price,img,description}=service;
+const Service = ({ service }) => {
+    const { id, name, price, img, description } = service;
     const navigate = useNavigate();
     const navigateToServiceDetail = id => {
         navigate(`/service/${id}`);
     }
     return (
-        <div className='service'>
-            <img src={img} alt="" />
-            <h2>{name}</h2>
-            <p>Price:{price}</p>
-            <p>Description:{description}</p>
-            <button onClick={()=>navigateToServiceDetail(id)} className='btn btn-primary'>Book: {name}</button>
-        </div>
+        // <div className='service'>
+        //     <img src={img} alt="" />
+        //     <h4>{name}</h4>
+        //     <h3>${price}</h3>
+        //     <p>{description}</p>
+        //     <button onClick={()=>navigateToServiceDetail(id)} className='btn btn-primary'>Book: {name}</button>
+        // </div>
+        <Card className='container service' style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={img} />
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>{description}</Card.Text>
+                <Button  onClick={()=>navigateToServiceDetail(id)} variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
     );
 };
 
